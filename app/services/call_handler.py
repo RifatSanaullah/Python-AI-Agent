dd91d258e3a22e6913efab7a9c6539acbd6fcde4
Rahat Saqib <rahatsaqib78@gmail.com>
2024-12-11 21:25:08 +0600

    added streaming and queueing for stream id

--- app/services/call_handler.py
+++ app/services/call_handler.py
@@ -23,14 +23,13 @@ class CallHandler:
         self.stream_sid = None
         self.polly_service = PollyService()
         self.deepgram_transcribe_service = DeepgramService(on_transcript=self.handle_transcript, on_start=self.on_user_speech)
-        self.transcribe_service = TranscribeService(on_transcript=self.handle_transcript)
-        self.max_chunk_size = 200
+        # self.transcribe_service = TranscribeService(on_transcript=self.handle_transcript)
         self.background_sound = False
         self.ai_speaking = False
 
     async def process_input(self, websocket):
         self.websocket = websocket
-        self.deepgram_transcribe_service.establishDGConnection()
+        self.deepgram_transcribe_service.establish_dg_connection()
         await websocket.accept()
         try:
             while True:
@@ -42,19 +41,19 @@ class CallHandler:
                     media = data["media"]
                     chunk = media["payload"]
                     chunk_bytes = base64.b64decode(chunk)
-                    await self.twilio_service.audio_buffer.put(chunk_bytes) 
+                    await self.twilio_service.enqueue_audio(self.stream_sid,chunk_bytes ,'audio_buffer')
 
-                if not self.twilio_service.response_buffer.empty():
+                if self.stream_sid and not self.twilio_service.is_empty(self.stream_sid, 'response_buffer'):
                     print("Processing response buffers...")
-                    response_audio = await self.twilio_service.response_buffer.get()
+                    response_audio = await self.twilio_service.get_or_dequeue_audio(self.stream_sid, 'response_buffer')
                     # await self.twilio_service.send_control_command(self.websocket, 'stop')
                     if self.background_sound is True:
                         await self.stop_stream()
                     self.ai_speaking = True
                     await self.twilio_service.send_audio_stream(self.websocket, self.stream_sid, response_audio)
 
-                if not self.twilio_service.audio_buffer.empty():
-                    audio_data = await self.twilio_service.audio_buffer.get()
+                if self.stream_sid and not self.twilio_service.is_empty(self.stream_sid, 'audio_buffer'):
+                    audio_data = await self.twilio_service.get_or_dequeue_audio(self.stream_sid, 'audio_buffer')
                     # await self.transcribe_service.transcribe(audio_data)
                     await self.deepgram_transcribe_service.transcribe(audio_data)
 
@@ -69,7 +68,10 @@ class CallHandler:
         finally:
             print("WebSocket connection closed.")
             await websocket.close()
-            self.transcribe_service.close()  # Close the transcriber service
+            # self.transcribe_service.close()  # Close the transcriber service
+            self.chatgpt_service.close_conversation(self.stream_sid)
+            self.twilio_service.remove_stream_from_queue(self.stream_sid)
+            self.deepgram_transcribe_service.disconnect()
             
     async def stop_stream(self):
         await self.twilio_service.stop_audio_stream(self.websocket, self.stream_sid)
@@ -83,7 +85,7 @@ class CallHandler:
     async def handle_transcript(self, transcript):
         print(f"Transcript: {transcript}")
         await self.enable_background_sound(True)
-        response = await self.chatgpt_service.generate_response(transcript)
+        response = await self.chatgpt_service.generate_response(self.stream_sid, transcript, self.synthesize_response)
         print(f"Response: {response}")
         await self.synthesize_response(response)
 
@@ -102,28 +104,21 @@ class CallHandler:
         return chunks
     
     async def synthesize_response(self, text: str):
-        # Chunk the text into smaller parts
-        text_chunks = self.chunk_text(text, self.max_chunk_size)
-        
-        # Synthesize audio for each chunk
-        for i, chunk in enumerate(text_chunks):
-            print(f"\nProcessing chunk {i + 1}...{chunk}\n")
 
-            start_time = datetime.now()
-            # audio_stream = await self.polly_service.stream_text_to_speech(chunk)
-            audio_stream = await self.deepgram_transcribe_service.stream_text_to_speech(chunk)
-            end_time = datetime.now()
-            duration = (end_time - start_time).total_seconds() * 1000  # Calculate duration in milliseconds
-            logging.info(f"Total Deepgram duration: {duration:.3f} ms")
-            await self.twilio_service.response_buffer.put(audio_stream)
+        start_time = datetime.now()
+        # audio_stream = await self.polly_service.stream_text_to_speech(chunk)
+        audio_stream = await self.deepgram_transcribe_service.stream_text_to_speech(text)
+        end_time = datetime.now()
+        duration = (end_time - start_time).total_seconds() * 1000  # Calculate duration in milliseconds
+        logging.info(f"Total Deepgram duration: {duration:.3f} ms")
+        await self.twilio_service.enqueue_audio(self.stream_sid, audio_stream ,'response_buffer')
 
         print('audio streamed')
         
     async def handle_call(self, call_id: str):
         print("Handling call...")
         response = self.twilio_service.initialize_call(call_id)
-        self.transcribe_service.connect()  # Connect the transcriber service
-        await self.synthesize_response("Hello and Welcome to BoomersHub!!")
+        # self.transcribe_service.connect()  # Connect the transcriber service
         return response
 
     async def make_outgoing_call(self, phone_number: str):
@@ -136,6 +131,7 @@ class CallHandler:
         call_sid = data.get("CallSid")
         print(f"Stream SID: {stream_sid}, Call SID: {call_sid}")
         self.stream_sid = stream_sid
+        await self.synthesize_response("Hello and Welcome to BoomersHub!!")
         return "OK", 200
     
     async def enable_background_sound(self ,status = False):
--- app/services/chatgpt_service.py
+++ app/services/chatgpt_service.py
@@ -5,15 +5,57 @@ from app.config import settings
 class ChatGPTService:
     def __init__(self):
         openai.api_key = settings.chatgpt_api_key
+        self.conversations = {}
 
-    async def generate_response(self, message: str, knowledge_base: str = ""):
-        response = openai.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
+    # Function to add messages to a conversation
+    def add_message(self, conversation_id, role, content):
+        if conversation_id not in self.conversations:
+            print(f"Conversation ID {conversation_id} does not exist.")
+            return
+        
+        self.conversations[conversation_id].append({"role": role, "content": content})
+
+    def initial_message(self , conversation_id, knowledge_base: str = ""):
+        self.conversations[conversation_id] = [
                 {"role": "system", "content": knowledge_base},
                 {"role": "system", "content": "You are a helpful assistant."},
                 {"role": "system", "content": "Keep your responses helpful and respectful and in under 2 senternces if possible."},
-                {"role": "user", "content": message},
-            ],
+        ]
+
+    async def generate_response(self, conversation_id, message: str, synthesize_response, knowledge_base: str = ""):
+
+        if conversation_id not in self.conversations:
+            self.initial_message(conversation_id ,knowledge_base)
+        
+        # Add user input to conversation history
+        self.add_message(conversation_id, "user", message)
+        response = openai.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=self.conversations[conversation_id],
+            stream=True  # Enable streaming
         )
-        return response.choices[0].message.content
+        print("Assistant: ", end="", flush=True)  # Print the assistant's response incrementally
+        assistant_reply = ""
+
+        # Process the streamed chunks
+        for chunk in response:
+            if chunk.choices and chunk.choices[0].delta:
+                delta = chunk.choices[0].delta
+                if delta.content:
+                    val = delta.content
+                    print(val, end="", flush=True)  # Display the streamed text
+                    assistant_reply += val  # Save the full assistant response
+                    if len(assistant_reply) > 200:
+                        await synthesize_response(assistant_reply)
+                        assistant_reply=''
+                    
+        self.add_message(conversation_id, "assistant", assistant_reply)
+        return assistant_reply
+    
+        # Function to close a conversation
+    def close_conversation(self, conversation_id):
+        if conversation_id in self.conversations:
+            del self.conversations[conversation_id]
+            print(f"Conversation ID {conversation_id} is now closed.")
+        else:
+            print(f"Conversation ID {conversation_id} does not exist.")
