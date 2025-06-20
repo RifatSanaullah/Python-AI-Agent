ffeee651c304b771e5f8880f02056beb3edbfdfb
RahatSaqib <rahatsaqib78@gmail.com>
2025-06-20 15:58:14 +0600

    Refactor CallHandler to remove Microsoft Azure TTS from streaming response logic

--- app/services/call_handler.py
+++ app/services/call_handler.py
@@ -1141,7 +1141,7 @@ class CallHandler:
         self.sessions[call_id]['ai_interrupt'] =  False
         self.ai_service.update_interrupt_status(call_id, False)
         streamingResponse = True
-        if self.agents[self.sessions[call_id]['call_sid']]['TTS']['name'] == 'Elevenlabs' or self.agents[self.sessions[call_id]['call_sid']]['TTS']['name'] == 'Microsoft Azure':
+        if self.agents[self.sessions[call_id]['call_sid']]['TTS']['name'] == 'Elevenlabs':
             streamingResponse = False
         response = await self.ai_service.generate_response(call_id, transcript, self.synthesize_response, self.agents[self.sessions[call_id]['call_sid']]['aiClient'], self.sessions[call_id]['synthesis_service'].flush_sp_ws, streamingResponse)
         if 'End Call Message' in response or self.contains_any_word(transcript) or  self.contains_any_word(response):
